89d080da0e2a0d1745c55070cfa865535ddd1b1d
alph4b3th <brunoisaac2717@gmail.com>
2023-10-26 21:47:08 -0300

    :sparkles: feat: limite de ciclos por episódio; o modelo aprendeu a voar infinitamente sem nunca tentar pousar, mesmo com gamma = 1

--- main.py
+++ main.py
@@ -6,25 +6,34 @@ from neural import DeepQNetwork
 
 if __name__ == "__main__":
     env = gym.make("LunarLander-v2", render_mode="human")
-    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4,
+    agent = Agent(gamma=1, epsilon=1.0, batch_size=128, n_actions=4,
                   eps_end=1e-2, 
-                  input_dims=[8], lr=3e-3)
+                  input_dims=[8], lr=1e-4)
     
     scores, eps_history = [], []
-    n_games = 500
+    n_games = 5000
+    n_max_cycles = 500
 
     for eps in range (n_games):
         score = 0
         done = False
         observation, _ = env.reset()
+        fuel = 0
 
         while not done:
+            if fuel > n_max_cycles:
+                break
+            fuel += 1
+            print(f"fuel: {fuel}")
             action = agent.choose_action(observation)
             observation_, reward, done, _, info = env.step(action)
             score += reward
             agent.store_transaction(observation, action, reward, observation_, done)
             agent.learn()
             observation=observation_
+           
+          
+              
 
         
         scores.append(score)
