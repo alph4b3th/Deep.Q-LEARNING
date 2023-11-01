272b4a88df3ef6dd12bdb082e6d9b52fd7434a2e
alph4b3th <brunoisaac2717@gmail.com>
2023-11-01 04:32:46 -0300

    :sparkles: feat: reajuste da rede neural, mantendo a arquitetura

--- neural.py
+++ neural.py
@@ -1,15 +1,15 @@
 import torch
 
 class DeepQNetwork(torch.nn.Module):
-    def __init__(self, lr, inputs_dims, n_actions):
+    def __init__(self, observations, n_actions):
         super(DeepQNetwork, self).__init__()
-        self.inputs_dims = inputs_dims
+        self.observations = observations
         self.n_actions = n_actions
 
         self.activation = torch.nn.ReLU()
         self.network = torch.nn.Sequential(
              self.activation,
-             torch.nn.Linear(*self.inputs_dims, 500),
+             torch.nn.Linear(self.observations, 500),
              torch.nn.Dropout(0.2),
              self.activation,
              torch.nn.Linear(500, 512),
@@ -23,19 +23,9 @@ class DeepQNetwork(torch.nn.Module):
              torch.nn.Linear(256, n_actions)
         )
 
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
-        self.loss_fn = torch.nn.MSELoss()
-        
-
         self.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu') 
         self.to(self.device)
-
-        try:
-            torch.load("deep-q.pth")
-            print("model is loaded with sucefull")
-        except:
-            print("fail to load model. Model weights exits ?")
-
+   
     def forward(self, state):
         actions = self.network(state)
         return actions
