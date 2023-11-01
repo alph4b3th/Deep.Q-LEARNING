b6bf5895442cc99b4b4d8e9aee31c7ffd58100c7
alph4b3th <brunoisaac2717@gmail.com>
2023-11-01 04:33:18 -0300

    :sparkles: feat: novo código main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import gymnasium as gym
+import torch
 import numpy as np
 from agent import Agent
 from neural import DeepQNetwork
@@ -16,42 +17,60 @@ if __name__ == "__main__":
                     turbulence_power = 2.0,
                   )
     
-    agent = Agent(gamma=0.60, 
-                  epsilon=2.4e-1,
-                  batch_size=64,
-                  n_actions=4,
-                  eps_end=1e-4, 
-                  input_dims=[8],
-                  lr=1e-3,
-                  max_memory_size=100_000)
+    state, _ = env.reset()
+    n_observations = len(state)
+    agent = Agent(observations=n_observations, n_actions=env.action_space.n)
     
     env = RecordVideo(env=env, 
                       episode_trigger=trigger_episode, 
-                      video_folder="Z:\iatreinando\LunarLander-v2-pt-6"
+                      video_folder="Z:\iatreinando\LunarLander-v2-pt-7"
                      )
    
     scores, eps_history = [], []
     n_games = 16000
 
 
+
     for eps in range (n_games):
         score = 0
-        cycles = 0
         done = False
-        observation, _ = env.reset()
+        state, _ = env.reset()
+        device = agent.policy_net.device
+        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
 
-        while True:
+        while not done:
             env.render()
-         
-            action = agent.choose_action(observation)
-            observation_, reward, done, truncated, info = env.step(action)
-            if truncated or done:
-                break
-
+            action = agent.choose_action(state, env)
+            agent.decay_eps()
+            observation, reward, terminated, truncated, _ = env.step(action.item())
             score += reward
-            agent.store_transaction(observation, action, reward, observation_, done)
+            reward = torch.tensor([reward], device=device)
+            done = terminated or truncated
+            
+            next_state = None
+            if not terminated:
+                next_state = torch.tensor(
+                                        observation,
+                                        dtype=torch.float32, 
+                                        device=device
+                            )
+                
+                next_state = next_state.unsqueeze(0)
+                
+            agent.memory.push(state, action, next_state, reward)
+            state = next_state
             agent.learn()
-            observation=observation_
+            agent.target_net_state_dict = agent.target_net.state_dict()
+            agent.policy_net_state_dict = agent.policy_net.state_dict()
+            for key in agent.policy_net_state_dict:
+                agent.target_net_state_dict[key] =\
+                agent.policy_net_state_dict[key]*agent.tau +\
+                agent.target_net_state_dict[key]*(1-agent.tau)
+                
+                agent.target_net.load_state_dict(agent.target_net_state_dict)
+
+            
+            
             
         
         scores.append(score)
